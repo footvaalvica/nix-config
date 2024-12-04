5be861a1d5c82491784fc543e0fd51dba4275209
Mateus Pinho <mateus.pinho@rnl.tecnico.ulisboa.pt>
2024-12-04 13:08:24 +0000

    update flakes and add steam

--- hosts/raidou/configuration.nix
+++ hosts/raidou/configuration.nix
@@ -43,6 +43,21 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
+  programs = {
+    java.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      gamescopeSession.enable = true;  
+    };
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+  };
+
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
