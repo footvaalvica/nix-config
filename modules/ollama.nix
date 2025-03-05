e4613a8e72592d33c7d40962f074b21e8d0546a1
Mateus Pinho <mateus.pinho@rnl.tecnico.ulisboa.pt>
2025-03-05 17:48:34 +0000

    add webui

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -12,5 +12,16 @@
   services.open-webui = {
     enable = true;
     openFirewall = true;
+    port = 11111;
   }
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."llm.footvaalvica.com".extraConfig = ''
+      reverse_proxy localhost:11111
+    '';
+    acmeCA = "https://acme-v02.api.letsencrypt.org/directory";
+  };
+
+  services.cloudflare-dyndns.domains = [ "llm.footvaalvica.com" ];
 }
