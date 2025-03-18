df2da881cf94fcdc2cf12f9bcc4c234e695b160e
Mateus Pinho <mateusleitepinho@gmail.com>
2025-03-18 11:54:50 +0000

    Enable remote build for NixOS configurations in flake.nix

--- flake.nix
+++ flake.nix
@@ -95,6 +95,7 @@
             user = "root";
             sshUser = "root";
             path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.raidou;
+            remoteBuild = true;
           };
         };
         omi = {
@@ -103,6 +104,7 @@
             user = "root";
             sshUser = "root";
             path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.omi;
+            remoteBuild = true;
           };
         };
       };
