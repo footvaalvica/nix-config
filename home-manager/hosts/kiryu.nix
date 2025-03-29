965c2c31faeaa592a5a51fdf1d9820318edc4fb1
Mateus Pinho <mateusleitepinho@gmail.com>
2025-03-29 19:08:00 +0000

    try this

--- home-manager/hosts/kiryu.nix
+++ home-manager/hosts/kiryu.nix
@@ -17,12 +17,20 @@
 
   targets.genericLinux.enable = true;
 
-  home.activation.setupOpenGLDriverSymlinks = config.lib.dag.entryAfter ["writeBoundary"] ''
+  # Activation script to manage OpenGL driver symlinks
+  home.activation.openglLink = pkgs.lib.hm.dag.entryAfter ["writeBoundary"] ''
+    echo "Setting up OpenGL driver symlinks..."
     OPENGL_PROFILE="/nix/var/nix/profiles/opengl-driver"
     OPENGL_PROFILE_32="/nix/var/nix/profiles/opengl-driver-32"
     OPENGL_SYMLINK_CONF="/etc/tmpfiles.d/nix-opengl-driver.conf"
-    sudo rm -f "$OPENGL_SYMLINK_CONF"
-    printf "L+ /run/opengl-driver - - - - %s\nL+ /run/opengl-driver-32 - - - - %s\n" "$OPENGL_PROFILE" "$OPENGL_PROFILE_32" | sudo tee "$OPENGL_SYMLINK_CONF"
-    sudo systemd-tmpfiles --create "$OPENGL_SYMLINK_CONF"
+    # Ensure commands use paths from Nix store for reproducibility
+    # Note: This requires the user running home-manager switch to have sudo privileges without a password for these specific commands,
+    # or for sudoers to be configured appropriately.
+    ${pkgs.sudo}/bin/sudo rm -f "$OPENGL_SYMLINK_CONF"
+    printf "L+ /run/opengl-driver - - - - %s\nL+ /run/opengl-driver-32 - - - - %s\n" "$OPENGL_PROFILE" "$OPENGL_PROFILE_32" | ${pkgs.sudo}/bin/sudo ${pkgs.coreutils}/bin/tee "$OPENGL_SYMLINK_CONF"
+    ${pkgs.sudo}/bin/sudo ${pkgs.systemd}/bin/systemd-tmpfiles --create "$OPENGL_SYMLINK_CONF"
+    echo "OpenGL symlink setup complete."
   '';
+
+
 }
