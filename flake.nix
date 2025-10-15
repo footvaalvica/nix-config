083fb1db32986eb2d6ca29d5eb359a9a8352c705
Mateus Pinho <mateusleitepinho@gmail.com>
2025-10-15 12:31:06 +0100

    go

--- flake.nix
+++ flake.nix
@@ -81,6 +81,14 @@
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
+    # Apps for convenient access to deploy-rs
+    apps = forAllSystems (system: {
+      deploy-rs = {
+        type = "app";
+        program = "${deploy-rs.packages.${system}.default}/bin/deploy";
+      };
+    });
+
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
     # Reusable nixos modules you might want to export
