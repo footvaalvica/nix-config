ef2ac19b05b408350ef8b01ddd0c110ecc75d41a
Mateus Pinho <mateusleitepinho@gmail.com>
2025-12-26 22:27:20 +0000

    try aga

--- modules/docker-containers/homeassistant.nix
+++ modules/docker-containers/homeassistant.nix
@@ -11,7 +11,7 @@
   virtualisation.oci-containers.backend = "docker";
 
   networking.firewall = {
-    allowedTCPPorts = [8123 8443 21063];
+    allowedTCPPorts = [8123 8443 21063 21064];
     allowedUDPPorts = [5353 ];
   };
 
