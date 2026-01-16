1b67992538d7f290a0141008c1e86e738b5786d1
Mateus Pinho <mateusleitepinho@gmail.com>
2026-01-16 10:59:30 +0000

    enable dnsmasq

--- hosts/omi/configuration.nix
+++ hosts/omi/configuration.nix
@@ -184,6 +184,8 @@
     };
   };
 
+  services.dnsmasq.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
