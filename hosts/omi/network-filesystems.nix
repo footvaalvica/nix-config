2e860e34c27c917f2a529fdda5adc750433ada61
Mateus Pinho <mateusleitepinho@gmail.com>
2026-03-15 19:17:18 +0000

    refactor backup configurations: replace CIFS with NFS for immich_backup and remove nextcloud_backup

--- hosts/omi/network-filesystems.nix
+++ hosts/omi/network-filesystems.nix
@@ -24,14 +24,7 @@
     fsType = "cifs";
     options = ["username=${secrets.smb.username}" "password=${secrets.smb.password}" "fsc" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "rw" "mfsymlinks" "seal" "uid=1000" "gid=100" "file_mode=0777" "dir_mode=0777"];
   };
-
-  # mount network drive as syncthing drive
-  fileSystems."/mnt/syncthing" = {
-    device = "//192.168.1.250/Mateus/Syncthing/";
-    fsType = "cifs";
-    options = ["username=${secrets.smb.username}" "password=${secrets.smb.password}" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "rw" "mfsymlinks" "seal" "uid=1000" "gid=100" "file_mode=0777" "dir_mode=0777"];
-  };
-
+  
   fileSystems."/mnt/backup" = {
     device = "//192.168.1.250/Mateus/Backup/";
     fsType = "cifs";
@@ -39,29 +32,9 @@
   };
 
   # Backups in StorageBox
-  fileSystems."/mnt/nextcloud_backup" = {
-    device = "//${secrets.hetzner.username}.your-storagebox.de/backup/NextCloudBackup";
-    fsType = "cifs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "rw"
-      "username=${secrets.hetzner.username}"
-      "password=${secrets.hetzner.password}"
-      "uid=33"
-      "gid=0"
-      "file_mode=0660"
-      "dir_mode=0770"
-    ];
-  };
-
-  # Backups in StorageBox
   fileSystems."/mnt/immich_backup" = {
-    device = "//${secrets.hetzner.username}.your-storagebox.de/backup/ImmichBackup";
-    fsType = "cifs";
+    device = "tojo:/mnt/backup/ImmichBackup";
+    fsType = "nfs";
     options = [
       "noauto"
       "x-systemd.automount"
@@ -69,12 +42,7 @@
       "x-systemd.device-timeout=5s"
       "x-systemd.mount-timeout=5s"
       "rw"
-      "username=${secrets.hetzner.username}"
-      "password=${secrets.hetzner.password}"
-      "uid=1000"
-      "gid=100"
-      "file_mode=0660"
-      "dir_mode=0770"
+      "soft" # Prevents omi from freezing if tojo goes offline
     ];
   };
 }
