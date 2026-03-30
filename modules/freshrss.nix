b7ab0595113d9ca949021f6625dde535875a2afd
Mateus Pinho <mateusleitepinho@gmail.com>
2026-03-30 20:39:37 +0100

    freshrss config

--- modules/freshrss.nix
+++ modules/freshrss.nix
@@ -2,32 +2,28 @@
   pkgs,
   lib,
   secrets,
+  config,
+  agenix,
   ...
 }: {
 
-  services.caddy = {
-    enable = true;
-    virtualHosts."freshrss.footvaalvica.com".extraConfig = ''
-      reverse_proxy localhost:5423
-    '';
-    acmeCA = "https://acme-v02.api.letsencrypt.org/directory";
-  };
-
   services.cloudflare-dyndns.domains = ["freshrss.footvaalvica.com"];
+
+  age.secrets.freshrss-password-file = {
+    file = ../secrets/freshrss.age;
+    mode = "777";
+  };
 
   services.freshrss = {
     enable = true;
-    openFirewall = true;
     webserver = "caddy";
     virtualHost = "freshrss.footvaalvica.com";
-    extensions = with freshrss-extensions; [
+    extensions = with pkgs.freshrss-extensions; [
       youtube
       reddit-image
     ];
-    api.enable = true;
-    baseUrl = "https://freshrss.footvaalvica.com";
-
-  
+    baseUrl = "https://freshrss.footvaalvica.com"; 
+	passwordFile = config.age.secrets.freshrss-password-file.path;
   };
 }
 
