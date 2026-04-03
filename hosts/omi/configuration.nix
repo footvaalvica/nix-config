dbfb7106b71fb5bce87e3c87e77ee36cc61d916e
Mateus Pinho <mateusleitepinho@gmail.com>
2026-04-03 17:37:27 +0100

    enable performance

--- hosts/omi/configuration.nix
+++ hosts/omi/configuration.nix
@@ -40,6 +40,8 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
+  powerManagement.cpuFreqGovernor = "performance";
+
   # Enable zram.
   networking.hostName = "omi"; # Define your hostname.
   hardware.bluetooth = {
