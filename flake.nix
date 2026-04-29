cf040eab2ddc31bc5640b0f8ba39f2d9243f922c
Mateus Pinho <mateusleitepinho@gmail.com>
2026-04-29 11:39:48 +0100

    G

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
     # Use `github:nix-darwin/nix-darwin/nix-darwin-25.11` to use Nixpkgs 25.11.
