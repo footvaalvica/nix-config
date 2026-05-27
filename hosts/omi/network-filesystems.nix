576b32a135a8d5cff4b9d2d8f9adb1c683872e52
Mateus Pinho <mateusleitepinho@gmail.com>
2026-05-27 10:22:39 +0100

    Format CIFS options and remove unused args

--- hosts/omi/network-filesystems.nix
+++ hosts/omi/network-filesystems.nix
@@ -1,9 +1,8 @@
 {
-  pkgs,
-  lib,
   secrets,
   ...
-}: {
+}:
+{
   # mount network drive as local drive for backups, and cache it as well
   services.cachefilesd = {
     enable = true;
@@ -17,19 +16,66 @@
   fileSystems."/mnt/nextcloud" = {
     device = "//192.168.1.250/Mateus/NextCloud";
     fsType = "cifs";
-    options = ["username=${secrets.smb.username}" "password=${secrets.smb.password}" "fsc" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "rw" "mfsymlinks" "seal" "uid=33" "gid=0" "file_mode=0777" "dir_mode=0777"];
+    options = [
+      "username=${secrets.smb.username}"
+      "password=${secrets.smb.password}"
+      "fsc"
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "rw"
+      "mfsymlinks"
+      "seal"
+      "uid=33"
+      "gid=0"
+      "file_mode=0777"
+      "dir_mode=0777"
+    ];
   };
 
   fileSystems."/mnt/immich" = {
     device = "//192.168.1.250/Mateus/Immich";
     fsType = "cifs";
-    options = ["username=${secrets.smb.username}" "password=${secrets.smb.password}" "fsc" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "rw" "mfsymlinks" "seal" "uid=1000" "gid=100" "file_mode=0777" "dir_mode=0777"];
+    options = [
+      "username=${secrets.smb.username}"
+      "password=${secrets.smb.password}"
+      "fsc"
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "rw"
+      "mfsymlinks"
+      "seal"
+      "uid=1000"
+      "gid=100"
+      "file_mode=0777"
+      "dir_mode=0777"
+    ];
   };
 
   fileSystems."/mnt/borg" = {
     device = "//192.168.1.250/Mateus/Backup/";
     fsType = "cifs";
-    options = ["username=${secrets.smb.username}" "password=${secrets.smb.password}" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "rw" "mfsymlinks" "seal" "uid=1000" "gid=100" "file_mode=0777" "dir_mode=0777"];
+    options = [
+      "username=${secrets.smb.username}"
+      "password=${secrets.smb.password}"
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "rw"
+      "mfsymlinks"
+      "seal"
+      "uid=1000"
+      "gid=100"
+      "file_mode=0777"
+      "dir_mode=0777"
+    ];
   };
 
   services.borgbackup = {
