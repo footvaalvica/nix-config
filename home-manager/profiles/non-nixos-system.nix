cfd047b5c0768467ca8587c3501d09a433ffbdc2
Mateus Pinho <mateusleitepinho@gmail.com>
2026-06-01 16:12:55 +0100

    add

---
+++ home-manager/profiles/non-nixos-system.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  targets.genericLinux.enable = true;
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/mateusp/nix-config"; # sets NH_OS_FLAKE variable for you
+  };
+}
