634c1da2098c8c4198cd872f4023e1483363e087
Mateus Pinho <mateusleitepinho@gmail.com>
2026-06-20 12:57:39 +0100

    remove waylander

--- home-manager/hosts/kiryu.nix
+++ home-manager/hosts/kiryu.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
@@ -31,44 +30,44 @@
     };
   };
 
-  # THIS MIGHT BREAK IF VALVE UPDATES KDE TO WAYLAND ONLY???? HOPE THEY DONT
-  home.file.".Xmodmap".text = ''
-    keycode 108 = Super_R
-    keycode 134 = ISO_Level3_Shift
+  # # THIS MIGHT BREAK IF VALVE UPDATES KDE TO WAYLAND ONLY???? HOPE THEY DONT
+  # home.file.".Xmodmap".text = ''
+  #   keycode 108 = Super_R
+  #   keycode 134 = ISO_Level3_Shift
 
-    clear mod4
-    clear mod5
+  #   clear mod4
+  #   clear mod5
 
-    add mod4 = Super_R
-    add mod5 = ISO_Level3_Shift
-  '';
+  #   add mod4 = Super_R
+  #   add mod5 = ISO_Level3_Shift
+  # '';
 
-  home.file.".xprofile".text = ''
-    if ${pkgs.xinput}/bin/xinput list --name-only | ${pkgs.gnugrep}/bin/grep -qi 'keychron'; then
-      ${pkgs.setxkbmap}/bin/setxkbmap
-    else
-      ${pkgs.xmodmap}/bin/xmodmap ~/.Xmodmap
-    fi
-  '';
+  # home.file.".xprofile".text = ''
+  #   if ${pkgs.xinput}/bin/xinput list --name-only | ${pkgs.gnugrep}/bin/grep -qi 'keychron'; then
+  #     ${pkgs.setxkbmap}/bin/setxkbmap
+  #   else
+  #     ${pkgs.xmodmap}/bin/xmodmap ~/.Xmodmap
+  #   fi
+  # '';
 
-  xdg.configFile."autostart-scripts/keychron-keymap.sh" = {
-    executable = true;
-    text = ''
-      #!${pkgs.bash}/bin/bash
-      sleep 2
+  # xdg.configFile."autostart-scripts/keychron-keymap.sh" = {
+  #   executable = true;
+  #   text = ''
+  #     #!${pkgs.bash}/bin/bash
+  #     sleep 2
 
-      log="$HOME/.local/state/keychron-keymap.log"
-      ${pkgs.coreutils}/bin/mkdir -p "$(${pkgs.coreutils}/bin/dirname "$log")"
+  #     log="$HOME/.local/state/keychron-keymap.log"
+  #     ${pkgs.coreutils}/bin/mkdir -p "$(${pkgs.coreutils}/bin/dirname "$log")"
 
-      if ${pkgs.xinput}/bin/xinput list --name-only | ${pkgs.gnugrep}/bin/grep -qi 'keychron'; then
-        ${pkgs.setxkbmap}/bin/setxkbmap
-        printf '%s Keychron detected: ran setxkbmap\n' "$(${pkgs.coreutils}/bin/date -Is)" >> "$log"
-      else
-        ${pkgs.xmodmap}/bin/xmodmap ~/.Xmodmap
-        printf '%s Keychron not detected: ran xmodmap\n' "$(${pkgs.coreutils}/bin/date -Is)" >> "$log"
-      fi
-    '';
-  };
+  #     if ${pkgs.xinput}/bin/xinput list --name-only | ${pkgs.gnugrep}/bin/grep -qi 'keychron'; then
+  #       ${pkgs.setxkbmap}/bin/setxkbmap
+  #       printf '%s Keychron detected: ran setxkbmap\n' "$(${pkgs.coreutils}/bin/date -Is)" >> "$log"
+  #     else
+  #       ${pkgs.xmodmap}/bin/xmodmap ~/.Xmodmap
+  #       printf '%s Keychron not detected: ran xmodmap\n' "$(${pkgs.coreutils}/bin/date -Is)" >> "$log"
+  #     fi
+  #   '';
+  # };
 
   programs.nh.flake = lib.mkForce "${config.home.homeDirectory}/nix-config";
   programs.nh.homeFlake = lib.mkForce "${config.home.homeDirectory}/nix-config/";
