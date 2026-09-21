b98a7b97f05c519d750b6e440faea914c9e69954
Mateus Pinho <mateusleitepinho@gmail.com>
2026-09-21 20:04:44 +0100

    remove intel stuff on sonic

--- hosts/sonic/sonic.nix
+++ hosts/sonic/sonic.nix
@@ -28,9 +28,6 @@
     # Install Homebrew under the default prefix
     enable = true;
 
-    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-    enableRosetta = true;
-
     # User owning the Homebrew prefix
     user = "mateusp";
 
